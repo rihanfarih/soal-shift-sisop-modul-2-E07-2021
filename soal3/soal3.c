556a717593385c7c24be67aa892f7a8c3bb225c9
rihanfarih <55140308+rihanfarih@users.noreply.github.com>
2021-04-17 15:23:39 +0700

    +caesar cipher

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -1,1 +1,40 @@
-test
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <string.h>
+#include <time.h>
+#include <wait.h>
+
+//3e
+//caesar cipher adaptasi dari https://www.thecrazyprogrammer.com/2016/11/caesar-cipher-c-c-encryption-decryption.html
+void caesar(char message[], int key)
+{
+    char ch;
+    for (int i = 0; message[i] != '\0'; ++i)
+    {
+        ch = message[i];
+        if (ch >= 'a' && ch <= 'z')
+        {
+            ch = ch + key;
+            if (ch > 'z')
+            {
+                ch = ch - 'z' + 'a' - 1;
+            }
+            message[i] = ch;
+        }
+        else if (ch >= 'A' && ch <= 'Z')
+        {
+            ch = ch + key;
+            if (ch > 'Z')
+            {
+                ch = ch - 'Z' + 'A' - 1;
+            }
+            message[i] = ch;
+        }
+    }}
+    
